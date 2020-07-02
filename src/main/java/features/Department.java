5e6d22c81e9a8ae53f5c8ff9fd161efffe172752
Akshaya Sawant <akshayagsawant@gmail.com>
2020-07-02 15:13:36 +0530

    Creation of Immutable custom class

---
+++ src/main/java/features/Department.java
@@ -0,0 +1,31 @@
+package features;
+
+public final class Department {
+	private final String name;
+	private final String specialization;
+
+	public Department(String name, String specialization) {
+		super();
+		this.name = name;
+		this.specialization = specialization;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public String getSpecialization() {
+		return specialization;
+	}
+
+	public Department clone() {
+		Department deptC = new Department(this.name,this.specialization);
+		return deptC;
+	}
+
+	@Override
+	public String toString() {
+		return "Department [name=" + name + ", specialization=" + specialization + "]";
+	}
+
+}
