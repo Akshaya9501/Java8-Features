cbbfed63d6a90fb483795523b2e15af48ab8ad2b
Akshaya9501 <akshayagsawant@gmail.com>
2020-07-06 14:16:32 +0530

    Functional Intefaces implementation

---
+++ src/main/java/java8/StudentDatabase.java
@@ -0,0 +1,16 @@
+package java8;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class StudentDatabase {
+
+	public static List<Student> addStudents() {
+		List<Student> studentList = new ArrayList<Student>();
+		studentList.add(new Student(1, "Rijul", 4));
+		studentList.add(new Student(2, "Sneha", 5));
+		studentList.add(new Student(3, "Farah", 4));
+		studentList.add(new Student(4, "Ira", 1));
+		return studentList;
+	}
+}
